7a7ed7e7c67d59cfa33e446b21c93c568a8a6755
BooLoo Lab <booloolab@gmail.com>
2020-10-06 22:27:00 +0800

    update wp-config with greengeeks server db settings

--- wp-config.php
+++ wp-config.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2633f9b8d1837f9fc2d18b7acb439591066100d38802a9abb45eb6600435eb23
-size 3184
+oid sha256:bb9666b4db98ab777a7bbe67da4aca6b7518678c29dde3ec76dacd1979bb75c1
+size 3214
